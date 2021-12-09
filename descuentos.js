295b5dc7e59bd40e28e61b96f0cfa6f44320d736
Jorge Borda <george.bordalopez@gmail.com>
2021-12-09 17:15:08 -0500

    Cambio de descuento fijo a cupones

--- descuentos.js
+++ descuentos.js
@@ -8,15 +8,42 @@ function calcularPrecioConDescuento(precio, descuento){
 function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCupon = document.getElementById("InputCupon");
+    const cuponValue = inputCupon.value;
+    var descuento;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    switch(cuponValue){
+        case cupon[0]:
+            descuento = 0.1;
+        break;
+        case cupon[1]:
+            descuento = 0.2;
+        break;
+        case cupon[2]:
+            descuento = 0.1;
+        break;
+        case cupon[3]:
+            descuento = 0.1;
+        break;
+        default:
+            descuento = 0;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento es $" + precioConDescuento;
-    }    
-//const precioOriginal = 100;
+    }
+
+var cupon = [
+    "Batman",
+    "Superman",
+    "Aquaman",
+    "Flash"
+]
+
+    //const precioOriginal = 100;
 
 //const descuento = 0.15;
 
