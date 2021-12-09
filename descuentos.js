f787cf08265f55091dcec64ecb6360bb1d5e693b
Jorge Borda <george.bordalopez@gmail.com>
2021-12-09 05:39:11 -0500

    Formulario para ingreso de datos y sobreescritura de html por medio de js

--- descuentos.js
+++ descuentos.js
@@ -1,13 +1,29 @@
-const precioOriginal = 100;
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 1 - descuento;
+    const precioConDescuento = precio * porcentajePrecioConDescuento;
+    
+    return precioConDescuento;
+}
 
-const descuento = 0.15;
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
 
-const porcentajePrecioConDescuento = 1 - descuento;
-const precioConDescuento = precioOriginal * porcentajePrecioConDescuento;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
 
-console.log({
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento es $" + precioConDescuento;
+    }    
+//const precioOriginal = 100;
+
+//const descuento = 0.15;
+
+
+/*console.log({
     precioOriginal,
     descuento,
     porcentajePrecioConDescuento,
     precioConDescuento
-});
+});*/
